81b6c4ec016a19cd96edaa383512a8d4e469b65f
liubov-lebedeva <lebed.lyuba@gmail.com>
2024-01-20 15:17:18 +0200

    Refactored structure of main branch where created the index.html and will create all homework projects. Also, console.log changed by alert to complete task.

---
+++ hw2/js/main.js
@@ -0,0 +1,3 @@
+let yourName= prompt("What is your name?")
+
+alert(`Hello, ${yourName}! How are you?`)
--- js/main.js
+++
@@ -1,3 +0,0 @@
-let yourName= prompt("What is your name?")
-
-console.log(`Hello, ${yourName}! How are you?`)
