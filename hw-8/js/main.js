54c65f860975e13e1746b1c7afd6c70363cca638
liubov-lebedeva <lebed.lyuba@gmail.com>
2024-02-07 16:53:00 +0200

    Completed second task in HW-8.

--- hw-8/js/main.js
+++ hw-8/js/main.js
@@ -1,9 +1,19 @@
+//first task
 let resultString = "";
-for (let i= 20; i <= 30; i = i + 0.5 ) {
-    if (i != 30) {
+for (let i = 20; i <= 30; i = i + 0.5) {
+    if (i !== 30) {
         resultString = resultString + i + " ";
     } else {
         resultString = resultString + i;
     }
+}
+alert(resultString);
+
+//second task
+const oneDollar = 27;
+resultString = "";
+for (let i = 10; i <= 100; i = i + 10) {
+    let dollarExchangeRate = oneDollar * i;
+    resultString += dollarExchangeRate + " ";
 }
 alert(resultString);
