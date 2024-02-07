8131d33f4e1f3133cd6a1a34d7674baf810bdfdd
liubov-lebedeva <lebed.lyuba@gmail.com>
2024-02-07 17:25:12 +0200

    Completed HW-8 tasks with advanced loops.

--- hw-8/js/main.js
+++ hw-8/js/main.js
@@ -31,16 +31,33 @@ alert(resultString);
 //forth task
 const primeNumber = Number(prompt("Input integer number:"));
 let numberIsPrime = true;
-for (let i = 2; i < Math.sqrt(primeNumber); i++) {
+for (let i = 2; i <= Math.sqrt(primeNumber); i++) {
     if (primeNumber % i === 0) {
         numberIsPrime = false;
         break;
     }
 }
-if (numberIsPrime) {
-    resultString = "The number is prime";
-    alert(resultString);
-} else {
-    resultString = "The number is not prime";
-    alert(resultString);
+resultString = numberIsPrime ? "The number is prime" : "The number is not prime";
+alert(resultString);
+
+
+//fifth task
+const inputNumber = Number(prompt("Input integer number:"));
+let isPowerOfThree = false;
+let i = 1;
+let power;
+while (true) {
+    let j = 3 ** i;
+    if (j === inputNumber) {
+        isPowerOfThree = true;
+        power = i;
+        break;
+    }
+    if (j > inputNumber) {
+        break;
+    }
+    i++;
 }
+resultString = isPowerOfThree ? `3^${power} = ${inputNumber}` : `${inputNumber} is not power of 3`;
+alert(resultString);
+
