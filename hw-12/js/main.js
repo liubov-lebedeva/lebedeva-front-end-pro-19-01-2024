7526cbfb17a244639412e04049e219b841d5cf71
liubov-lebedeva <lebed.lyuba@gmail.com>
2024-02-09 19:19:09 +0200

    Completed HW-12 task with closure.

---
+++ hw-12/js/main.js
@@ -0,0 +1,10 @@
+let sum = 0;
+function sumOfNumbers(num) {
+    sum += num;
+    return sum;
+}
+
+console.log(sumOfNumbers(3));
+console.log(sumOfNumbers(5));
+console.log(sumOfNumbers(20));
+console.log(sumOfNumbers(40));
