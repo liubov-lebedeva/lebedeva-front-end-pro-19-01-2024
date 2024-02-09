a727565a90c115e2c8e21ce95f3b773dc2ae9e65
liubov-lebedeva <lebed.lyuba@gmail.com>
2024-02-09 15:23:33 +0200

    Deleted unnecessary console.log.

--- hw-11/js/main.js
+++ hw-11/js/main.js
@@ -1,5 +1,4 @@
 const primeNumbers = [2, 3, 5, 5, 5, 5, 5, 5, 5, 5, 7, 11, 13, 17, 19, 23, 5, 5, 5, 5, 5, 29, 31, 37, 41, 47, 53, 59, 67, 71, 83, 89, 101, 107, 109, 113, 127];
-console.log(primeNumbers);
 
 let itemToRemove = 5;
 
