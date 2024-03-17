8ece37bd045aee854d0a699f1f26b8927ead9953
liubov-lebedeva <lebed.lyuba@gmail.com>
2024-03-17 20:18:11 +0200

    Added to HW-21 second dynamically created table with random filling from 1 to 100.

--- hw-21/js/main.js
+++ hw-21/js/main.js
@@ -16,25 +16,25 @@ const show = () => {
 };
 window.addEventListener('load', show);
 
-// const secondShow = () => {
-//     let body = document.querySelector('body');
-//     let table = document.querySelector('.table-2');
-//     let array = Array.from({length: 100}, (_, i) => i + 1);
-//     let element;
-//
-//
-//     for (let i = 0; i < 10; i++) {
-//         let tr = document.createElement('tr');
-//         for (let j = 0; j < 10; j++) {
-//             let td = document.createElement('td');
-//             let randomIndex = Math.floor(Math.random() * array.length);
-//             randomIndex = +randomIndex;
-//             element = array[randomIndex];
-//             array.splice(randomIndex, 1);
-//             td.textContent = element.toString();
-//             tr.appendChild(td);
-//         }
-//         table.appendChild(tr);
-//     }
-// };
-// window.addEventListener('load', secondShow);
+const secondShow = () => {
+    let body = document.querySelector('body');
+    let table = document.querySelector('.table-2');
+    let array = Array.from({length: 100}, (_, i) => i + 1);
+    let element;
+
+
+    for (let i = 0; i < 10; i++) {
+        let tr = document.createElement('tr');
+        for (let j = 0; j < 10; j++) {
+            let td = document.createElement('td');
+            let randomIndex = Math.floor(Math.random() * array.length);
+            randomIndex = +randomIndex;
+            element = array[randomIndex];
+            array.splice(randomIndex, 1);
+            td.textContent = element.toString();
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+    }
+};
+window.addEventListener('load', secondShow);
