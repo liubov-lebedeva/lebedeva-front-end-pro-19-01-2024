f776a4d44de213371213b5c9572fd390d8f3991b
liubov-lebedeva <lebed.lyuba@gmail.com>
2024-03-17 21:00:00 +0200

    Optimized code with merging two functions into one. Now, I use arrays to fill tables and use shuffleArray function to randomize elements in second table.

--- hw-21/js/main.js
+++ hw-21/js/main.js
@@ -1,40 +1,31 @@
-const show = () => {
-    let body = document.querySelector('body');
-    let table = document.querySelector('.table-1');
+const show = (array, tableNumber) => {
+    let table = document.querySelector(tableNumber);
     let counter = 0;
 
     for (let i = 0; i < 10; i++) {
         let tr = document.createElement('tr');
         for (let j = 0; j < 10; j++) {
             let td = document.createElement('td');
+            td.textContent = array[counter].toString();
             counter++;
-            td.textContent = counter.toString();
             tr.appendChild(td);
         }
         table.appendChild(tr);
     }
 };
-window.addEventListener('load', show);
 
-const secondShow = () => {
-    let body = document.querySelector('body');
-    let table = document.querySelector('.table-2');
-    let array = Array.from({length: 100}, (_, i) => i + 1);
-    let element;
-
-
-    for (let i = 0; i < 10; i++) {
-        let tr = document.createElement('tr');
-        for (let j = 0; j < 10; j++) {
-            let td = document.createElement('td');
-            let randomIndex = Math.floor(Math.random() * array.length);
-            randomIndex = +randomIndex;
-            element = array[randomIndex];
-            array.splice(randomIndex, 1);
-            td.textContent = element.toString();
-            tr.appendChild(td);
-        }
-        table.appendChild(tr);
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
     }
-};
-window.addEventListener('load', secondShow);
+}
+
+let array1 = Array.from({length: 100}, (_, i) => i + 1);
+let array2 = Array.from({length: 100}, (_, i) => i + 1);
+shuffleArray(array2);
+window.addEventListener('load', () => {
+        show(array1, '.table-1');
+        show(array2, '.table-2');
+    }
+);
