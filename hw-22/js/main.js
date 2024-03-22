ab767d209b6e6057b2fd4e1009a02b8e3470994f
liubov-lebedeva <lebed.lyuba@gmail.com>
2024-03-22 17:15:46 +0200

    Completed HW-22 with random images.

---
+++ hw-22/js/main.js
@@ -0,0 +1,22 @@
+let getImage = (array) => {
+    const randomNumber = Math.floor(Math.random() * array.length);
+    return `<img src="${array[randomNumber]}" alt="img">`;
+}
+
+let insertImage = (image)  => {
+    let div = document.querySelector("div.image");
+    div.innerHTML = image;
+}
+
+
+let imagesArray = [];
+for (let i = 0; i < 9; i++) {
+    imagesArray[i] = `images/${i + 1}.jpg`;
+}
+
+window.addEventListener('load', () => {
+        let image = getImage(imagesArray);
+        insertImage(image);
+    }
+);
+
